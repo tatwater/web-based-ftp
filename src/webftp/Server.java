200a479e3a0e7700bfc9e0091325cdbf4d02227f
Teagan <tatwater@outlook.com>
2014-05-06 18:57:21 -0400

    Added some src files

---
+++ src/webftp/Server.java
@@ -0,0 +1,16 @@
+package webftp;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Server extends Thread {
+
+}
